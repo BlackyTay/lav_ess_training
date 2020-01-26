455732dc6fa393e8f58d3ad0e5105fe7ed695f91
Tay Zheng Qiang <tay@outlook.my>
2020-01-26 21:06:26 +0800

    Simplify routes by resource()

--- routes/web.php
+++ routes/web.php
@@ -23,10 +23,4 @@ Route::get('/test', function() {return "Goodbye";} );
 
 Route::get('/rooms', 'ShowRoomsController');
 
-Route::get('/bookings', 'BookingController@index');
-Route::get('/bookings/create', 'BookingController@create');
-Route::post('/bookings', 'BookingController@store');
-Route::get('/bookings/{booking}', 'BookingController@show'); 
-Route::get('/bookings/{booking}/edit', 'BookingController@edit');
-Route::put('/bookings/{booking}/', 'BookingController@update');
-Route::delete('/bookings/{booking}', 'BookingController@destroy');
+Route::resource('bookings', 'BookingController');
