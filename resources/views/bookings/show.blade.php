15fe2a022d7085b20f97e585161d97045e767856
Tay Zheng Qiang <tay@outlook.my>
2020-01-27 08:54:58 +0800

    ** Added show.blade.php (unknown error for getAttributes() for ) and modified show() in BookingController

---
+++ resources/views/bookings/show.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<dl>
+    @foreach($booking->getAttributes() as $name => $ value)
+    <dt>{{ $name }}</dt>
+    <dd>{{ $booking->$name }}</dd>
+    @endforeach
+</dl>
+@endsection
