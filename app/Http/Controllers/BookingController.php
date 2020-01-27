cd3193a327204b3bbdc704f90c4bc4d31a07759d
Tay Zheng Qiang <tay@outlook.my>
2020-01-27 16:59:17 +0800

    Modified store() to shorter Eloquent code

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -100,16 +100,18 @@ class BookingController extends Controller
      */
     public function update(Request $request, Booking $booking)
     {
-        DB::table('bookings')
-        ->where('id', $booking->id)
-        ->update([
-            'room_id' => $request->input('room_id'),
-            'start' => $request->input('start'),
-            'end' => $request->input('end'),
-            'is_reservation' => $request->input('is_reservation', false),
-            'is_paid' => $request->input('is_paid', false),
-            'notes' => $request->input('notes')
-        ]);
+        // DB::table('bookings')
+        // ->where('id', $booking->id)
+        // ->update([
+        //     'room_id' => $request->input('room_id'),
+        //     'start' => $request->input('start'),
+        //     'end' => $request->input('end'),
+        //     'is_reservation' => $request->input('is_reservation', false),
+        //     'is_paid' => $request->input('is_paid', false),
+        //     'notes' => $request->input('notes')
+        // ]);
+        $booking->fill($request->input());
+        $booking->save();
         DB::table('bookings_users')
         ->where('booking_id', $booking->id)
         ->update([
