9fc14e7f0c9dcfe2d6c85f5d9f68b070eb5190e7
Tay Zheng Qiang <zhengqiang@goodsane.com>
2020-01-30 17:56:11 +0800

    A save state to be deleted

--- app/Libraries/Notifications.php
+++ app/Libraries/Notifications.php
@@ -9,6 +9,6 @@ class Notifications implements NotificationsInterface
 {
     public function send()
     {
-        Mail::to('sample@test.com')->send(new Reservations('Alex Winger'));
+        Mail::to('sample@test.com')->send(new Reservations(['Alex Winger']));
     }
 }
