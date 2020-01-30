deba17dbd5ce732e6f60771027a2f3bfeecd6a1a
Tay Zheng Qiang <zhengqiang@goodsane.com>
2020-01-30 11:41:30 +0800

    Add codes for testUpdateFile with UploadedFile and Storage testing

--- tests/Feature/ShowRoomsControllerTest.php
+++ tests/Feature/ShowRoomsControllerTest.php
@@ -5,6 +5,8 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 
 class ShowRoomsControllerTest extends TestCase
 {
@@ -37,5 +39,17 @@ class ShowRoomsControllerTest extends TestCase
             ->assertViewIs('rooms.index')
             ->assertViewHas('rooms')
             ->assertSeeText($roomType->name);
+    }
+
+    public function testUpdateFile()
+    {
+        $file = UploadedFile::fake()->image('sample.jpg');
+        $roomType = factory('App\RoomType')->create();
+
+        $response = $this->put('/room_types/{$roomType->id}', ['picture' => $file]);
+
+        $response->assertStatus(302)
+            ->assertRedirect('/room_types');
+        Storage::disk('public')->assertExists($file->hashName());
     }
 }
