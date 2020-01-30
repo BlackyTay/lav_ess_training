323ffe99f11d03775c1c531f383a954de87cb192
Tay Zheng Qiang <zhengqiang@goodsane.com>
2020-01-30 10:39:30 +0800

    Add fake data format definition for factories

--- database/factories/RoomFactory.php
+++ database/factories/RoomFactory.php
@@ -6,7 +6,10 @@ use App\Room;
 use Faker\Generator as Faker;
 
 $factory->define(Room::class, function (Faker $faker) {
+    $roomType = DB::table('room_types')->pluck('id')->all();
+
     return [
-        //
+        'number' => $faker->unique()->randomNumber(),
+        'romm_type_id' => $faker->randomElement($roomTypes),
     ];
 });
--- database/factories/RoomTypeFactory.php
+++ database/factories/RoomTypeFactory.php
@@ -7,6 +7,8 @@ use Faker\Generator as Faker;
 
 $factory->define(RoomType::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->word(),
+        'description' => $faker->sentence(),
+        'deleted_at' => $faker->optional()->dateTime(),
     ];
 });
