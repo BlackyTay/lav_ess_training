81e856cf9898b169aab1a75819ade48a6528351e
Tay Zheng Qiang <zhengqiang@goodsane.com>
2020-01-30 11:19:37 +0800

    Import Facades.Cache

--- tests/Feature/RoomTypeControllerTest.php
+++ tests/Feature/RoomTypeControllerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Support\Facades\Cache;
 
 class RoomTypeControllerTest extends TestCase
 {
