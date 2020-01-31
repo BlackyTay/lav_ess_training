eebe01e31daa0599a902919756e14599f6bfb4a7
Tay Zheng Qiang <zhengqiang@goodsane.com>
2020-01-31 17:52:19 +0800

    Comment sample codes in webpack.mix.js and try npm run watch-poll

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,13 +13,13 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sourceMaps()
-    .extract(['vue'])
+    // .extract(['vue'])
     .version()
     .sass('resources/sass/app.scss', 'public/css');
 
-mix.browserSync('sync.test');
-mix.babel(['first.js', 'second.js'], 'all.js');
-mix.react('app.jsx', 'public/js');
+// mix.browserSync('sync.test');
+// mix.babel(['first.js', 'second.js'], 'all.js');
+// mix.react('app.jsx', 'public/js');
 // mix.less('path.less', 'public/css/less.css')
 //     .less('path2.less', 'public/css/less2.css');
 
