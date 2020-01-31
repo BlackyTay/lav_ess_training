3cfd07b64873343fb639849e223695062a6f9993
Tay Zheng Qiang <zhengqiang@goodsane.com>
2020-01-31 17:44:12 +0800

    Add version() to webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,6 +14,7 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sourceMaps()
     .extract(['vue'])
+    .version()
     .sass('resources/sass/app.scss', 'public/css');
 
 // mix.less('path.less', 'public/css/less.css')
